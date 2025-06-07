d7df02ee59dca42fa83e8e1a682d78e8eb221175
Leandro Mileski <leandrotinoco55@gmail.com>
2025-06-07 17:23:55 -0300

    feat: create `Deposit` entity

--- src/main/java/com/lenews/jbank/entities/Deposits.java
+++ src/main/java/com/lenews/jbank/entities/Deposits.java
@@ -2,6 +2,8 @@ package com.lenews.jbank.entities;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -14,9 +16,58 @@ public class Deposits {
     private UUID depositId;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_wallet_id")
-    private Wallet receiver;
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
 
+    @Column(name = "amount_deposited")
+    private BigDecimal ammountDeposited;
 
+    @Column(name = "deposit_date")
+    private LocalDateTime depositDate;
 
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    public Deposits() {
+    }
+
+    public UUID getDepositId() {
+        return depositId;
+    }
+
+    public void setDepositId(UUID depositId) {
+        this.depositId = depositId;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public BigDecimal getAmmountDeposited() {
+        return ammountDeposited;
+    }
+
+    public void setAmmountDeposited(BigDecimal ammountDeposited) {
+        this.ammountDeposited = ammountDeposited;
+    }
+
+    public LocalDateTime getDepositDate() {
+        return depositDate;
+    }
+
+    public void setDepositDate(LocalDateTime depositDate) {
+        this.depositDate = depositDate;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 }
