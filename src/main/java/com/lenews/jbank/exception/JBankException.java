82c7c9cf1051609a341e4c049fb40243c7335210
Leandro Mileski <leandrotinoco55@gmail.com>
2025-06-08 15:37:52 -0300

    feat: `GlobalExceptionHandler.java` to global application exception handle.

--- src/main/java/com/lenews/jbank/exception/JBankException.java
+++ src/main/java/com/lenews/jbank/exception/JBankException.java
@@ -1,5 +1,7 @@
 package com.lenews.jbank.exception;
 
+import org.springframework.http.ProblemDetail;
+
 public abstract class JBankException extends RuntimeException {
     public JBankException(String message) {
         super(message);
@@ -7,5 +9,12 @@ public abstract class JBankException extends RuntimeException {
 
     public JBankException(Throwable cause) {
         super(cause);
+    }
+
+    public ProblemDetail toProblemDetail() {
+        var pd = ProblemDetail.forStatus(500);
+        pd.setTitle("Internal Server Error");
+        pd.setDetail("Contact JBank support for more information.");
+        return pd;
     }
 }
